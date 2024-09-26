f79bb2e8686824d79ca926f25cd9765a65b0e08d
Anshuman <anshuman0042@gmail.com>
2024-09-26 17:55:37 +0530

    this was the dequeue file addition

---
+++ dequeue.cpp
@@ -0,0 +1,20 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+     deque<int> dq;
+     dq.push_back(1);
+     dq.emplace_back(3);
+     dq.push_front(23);
+     dq.emplace_front(23);
+     for(auto& q:dq){
+        cout<<q<<"\n";
+     }
+     dq.pop_back();
+     dq.pop_front();
+     dq.back();
+     dq.front();
+     for(auto& q:dq){
+        cout<<q<<"\n";
+     }
+
+}
