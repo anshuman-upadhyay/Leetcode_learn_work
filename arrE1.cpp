492354cfaf7a3c466a976305147c9e664ea838c6
Anshuman <anshuman0042@gmail.com>
2024-11-02 23:24:58 +0530

    sorry for the delay

---
+++ arrE1.cpp
@@ -0,0 +1,23 @@
+//Largest element 
+#include<bits/stdc++.h>
+using namespace std;
+int largest(vector <int > &arr){
+    int max=0;
+    for(int i =0;i<arr.size();i++){
+        if(max<arr[i]){
+            max=arr[i];
+        }
+    }
+    return max;
+}
+
+
+
+int main(){
+    vector<int> a{1, 8, 7, 56, 90};
+    int max=largest(a);
+    cout<<max;
+
+cout<<endl;
+return 0;
+}
