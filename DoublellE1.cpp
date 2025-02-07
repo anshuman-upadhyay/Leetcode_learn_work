1e2f6b6ad3267ce68a544c634ae0325bb80714f3
Anshuman <anshuman0042@gmail.com>
2025-02-07 16:18:12 +0530

    Doubly linked list here we go

--- DoublellE1.cpp
+++ DoublellE1.cpp
@@ -4,20 +4,34 @@ class Node{
     public:
     int data;
     Node* next;
-    Node* previous;
+    Node* prev;
 
     Node(int d) {
         data=d;
         next=nullptr;
-        previous=nullptr;
+        prev=nullptr;
     }
 
     Node(int d,Node* next1,Node* pre1) {
         data=d;
         next= next1;
-        previous=pre1;
+        prev=pre1;
     }
 };
+Node* constructDLL(vector<int>& arr) {
+    int s=arr.size();
+    if(s==0){return nullptr;}
+    Node* head=new Node(arr[0]);
+    Node* mover=head;
+    for(int i=1;i<s;i++){
+        Node* temp=new Node(arr[i]);
+        mover->next=temp;
+        temp->prev=mover;
+        mover=temp;
+    }
+    return head;
+}
+
 void printLL(Node* node){
     Node* current=node;
     while(current!=NULL){
@@ -27,7 +41,8 @@ void printLL(Node* node){
 
 }
 int main(){
-
+    vector<int> nums={1,2,3,4,5};
+    Node* head=constructDLL(nums);
     printLL(head);
 cout<<endl;
 return 0;
